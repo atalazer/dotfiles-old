60688b880698b6f7f37304f1678e056c44c346eb
Atalazer <baraboyolali@gmail.com>
2021-03-23 19:45:13 +0700

    Bit Config

--- nvim/.config/nvim/lua/base/appearances.lua
+++ nvim/.config/nvim/lua/base/appearances.lua
@@ -1,8 +1,8 @@
 local cmd = vim.cmd
 
 -- xresources, ayu
-local colorscheme = "xresources"
-local ayu_theme = "dark"                    -- dark, light, mirage
+local colorscheme = "ayu"
+local ayu_theme = "mirage"                    -- dark, light, mirage
 
 cmd("syntax enable")
 cmd("let ayu_theme=\"" .. ayu_theme .. "\"")
