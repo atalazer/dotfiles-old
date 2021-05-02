37817d9d3713ff99a07c2e4fb57ebb272433ed9b
Atalazer <baraboyolali@gmail.com>
2021-05-02 13:18:24 +0700

    fix(awesomeWM): Temperatur Bar nil value

--- home/.config/awesome/evil/temperature.lua
+++ home/.config/awesome/evil/temperature.lua
@@ -4,10 +4,16 @@
 local awful = require("awful")
 
 local update_interval = 15
+
+-- @NOTE: Please Change #temp_script based on your machine
+
+-- local temp_script = [[
+--     sh -c " sensors | grep Package | awk '{print $4}' | cut -c 2-3 "
+-- ]]
+
 local temp_script = [[
-  sh -c "
-  sensors | grep Package | awk '{print $4}' | cut -c 2-3
-  "]]
+    sh -c "sensors | grep temp1 | sed -n 3p | awk '{print $2}' | cut -c 2-3"
+]]
 
 -- Periodically get temperature info
 awful.widget.watch(temp_script, update_interval, function(widget, stdout)
