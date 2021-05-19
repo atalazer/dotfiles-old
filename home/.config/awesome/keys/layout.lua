64726f693df123462a59ccc71565300479ef550d
Atalazer <baraboyolali@gmail.com>
2021-05-19 11:22:40 +0700

    chore: edit awesomeWM layout keys

--- home/.config/awesome/keys/layout.lua
+++ home/.config/awesome/keys/layout.lua
@@ -5,102 +5,143 @@ local helpers = require("helpers")
 
 local keys = gears.table.join(
     -- Gaps
-    awful.key({ superkey, shiftkey }, "minus",
-        function ()
-            awful.tag.incgap(5, nil)
-        end,
-        {description = "increment gaps size for the current tag", group = "gaps"}
-    ),
-    awful.key({ superkey }, "minus",
-        function ()
-            awful.tag.incgap(-5, nil)
-        end,
-        {description = "decrement gap size for the current tag", group = "gaps"}
-    ),
+    awful.key({ superkey, shiftkey }, "minus", function()
+        awful.tag.incgap(5, nil)
+    end, {
+        description = "increment gaps size for the current tag",
+        group = "gaps",
+    }),
+    awful.key({ superkey }, "minus", function()
+        awful.tag.incgap(-5, nil)
+    end, {
+        description = "decrement gap size for the current tag",
+        group = "gaps",
+    }),
 
     -- Resize focused client or layout factor
-    awful.key({ superkey, shiftkey }, "Down", function ()
+    awful.key({ superkey, shiftkey }, "Down", function()
         helpers.resize_dwim(client.focus, "down")
-    end),
-    awful.key({ superkey, shiftkey }, "j", function ()
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
+    awful.key({ superkey, shiftkey }, "j", function()
         helpers.resize_dwim(client.focus, "down")
-    end),
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, shiftkey }, "Up", function ()
+    awful.key({ superkey, shiftkey }, "Up", function()
         helpers.resize_dwim(client.focus, "up")
-    end),
-    awful.key({ superkey, shiftkey }, "k", function ()
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
+    awful.key({ superkey, shiftkey }, "k", function()
         helpers.resize_dwim(client.focus, "up")
-    end),
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, shiftkey }, "Left", function ()
+    awful.key({ superkey, shiftkey }, "Left", function()
         helpers.resize_dwim(client.focus, "left")
-    end),
-    awful.key({ superkey, shiftkey }, "h", function ()
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
+    awful.key({ superkey, shiftkey }, "h", function()
         helpers.resize_dwim(client.focus, "left")
-    end),
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, shiftkey }, "Right", function ()
+    awful.key({ superkey, shiftkey }, "Right", function()
         helpers.resize_dwim(client.focus, "right")
-    end),
-    awful.key({ superkey, shiftkey }, "l", function ()
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
+    awful.key({ superkey, shiftkey }, "l", function()
         helpers.resize_dwim(client.focus, "right")
-    end),
+    end, {
+        description = "Resize Client",
+        group = "layout",
+    }),
 
     -- Number of master clients
-    awful.key({ superkey, altkey }, "h",
-        function ()
-            awful.tag.incnmaster( 1, nil, true)
-        end,
-        {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ superkey, altkey }, "h", function()
+        awful.tag.incnmaster(1, nil, true)
+    end, {
+        description = "increase the number of master clients",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, altkey }, "l",
-        function ()
-            awful.tag.incnmaster(-1, nil, true)
-        end,
-        {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ superkey, altkey }, "l", function()
+        awful.tag.incnmaster(-1, nil, true)
+    end, {
+        description = "decrease the number of master clients",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, altkey }, "Left",
-        function ()
-            awful.tag.incnmaster( 1, nil, true)
-        end,
-        {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ superkey, altkey }, "Left", function()
+        awful.tag.incnmaster(1, nil, true)
+    end, {
+        description = "increase the number of master clients",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, altkey }, "Right",
-        function ()
-            awful.tag.incnmaster(-1, nil, true)
-        end,
-        {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ superkey, altkey }, "Right", function()
+        awful.tag.incnmaster(-1, nil, true)
+    end, {
+        description = "decrease the number of master clients",
+        group = "layout",
+    }),
 
     -- Number of columns
-    awful.key({ superkey, altkey }, "k",
-        function ()
-            awful.tag.incncol( 1, nil, true)
-        end,
-        {description = "increase the number of columns", group = "layout"}),
+    awful.key({ superkey, altkey }, "k", function()
+        awful.tag.incncol(1, nil, true)
+    end, {
+        description = "increase the number of columns",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, altkey }, "j",
-        function ()
-            awful.tag.incncol( -1, nil, true)
-        end,
-        {description = "decrease the number of columns", group = "layout"}),
+    awful.key({ superkey, altkey }, "j", function()
+        awful.tag.incncol(-1, nil, true)
+    end, {
+        description = "decrease the number of columns",
+        group = "layout",
+    }),
 
-    awful.key({ superkey, altkey }, "Up",
-        function ()
-            awful.tag.incncol( 1, nil, true)
-        end,
-        {description = "increase the number of columns", group = "layout"}),
-        
-    awful.key({ superkey, altkey }, "Down",
-        function ()
-            awful.tag.incncol( -1, nil, true)
-        end,
-        {description = "decrease the number of columns", group = "layout"})
+    awful.key({ superkey, altkey }, "Up", function()
+        awful.tag.incncol(1, nil, true)
+    end, {
+        description = "increase the number of columns",
+        group = "layout",
+    }),
 
-    --awful.key({ superkey,           }, "space", function () awful.layout.inc( 1)                end,
-    --{description = "select next", group = "layout"}),
-    --awful.key({ superkey, shiftkey   }, "space", function () awful.layout.inc(-1)                end,
-    --{description = "select previous", group = "layout"}),
+    awful.key({ superkey, altkey }, "Down", function()
+        awful.tag.incncol(-1, nil, true)
+    end, {
+        description = "decrease the number of columns",
+        group = "layout",
+    }),
+
+    awful.key({ superkey }, "space", function()
+        awful.layout.inc(1)
+    end, {
+        description = "select next",
+        group = "layout",
+    }),
+
+    awful.key({ superkey, shiftkey }, "space", function()
+        awful.layout.inc(-1)
+    end, {
+        description = "select previous",
+        group = "layout",
+    })
 
 )
 
